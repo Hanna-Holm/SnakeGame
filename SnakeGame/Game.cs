246832d777e1c74f68c6bea835c6e7e427d5d4b5
Hanna Holm <hanna.holm@hotmail.com>
2024-02-18 12:04:36 +0100

    Render window and rectangle shape which will act as the snake

--- SnakeGame/Game.cs
+++ SnakeGame/Game.cs
@@ -6,8 +6,46 @@ namespace SnakeGame
 {
     internal class Game
     {
+        private RenderWindow _window;
+        private RectangleShape _snake;
+
+        public Game()
+        {
+            // Setup window
+            _window = new RenderWindow(new VideoMode(800, 600), "SFML Window");
+            _snake = new RectangleShape(new Vector2f(10, 10))
+            {
+                FillColor = Color.Green,
+                Position = new Vector2f(100, 200)
+            };
+        }
+
         public void Run()
         {
+            // Main game loop
+            while (_window.IsOpen)
+            {
+                // Handle events
+                _window.DispatchEvents();
+
+                // Update game logic here
+
+                // Clear the window
+                _window.Clear(Color.Black);
+
+                // Draw game elements here
+                _window.Draw(_snake);
+
+                // Display the contents of the window
+                _window.Display();
+            }
+
+            // Add snake
+
+            // Make snake move every frame
+            // Be able to give input to move snake
+            // handle collisions with apple and walls
+
 
         }
     }
---
+++ SnakeGame/Snake.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    internal class Snake
+    {
+        public int Length = 10;
+        private bool _isAlive = true;
+
+        private void Eat(IEdible edible)
+        {
+            edible.GetEatenBy(this);
+        }
+
+        
+    }
+}
