23ac9c60dcf3604da11c73b71fa9a4525a34f918
Hanna Holm <hanna.holm@hotmail.com>
2024-02-25 11:26:44 +0100

    Implemented separate Snake class for all things related to the snake. Moved code from the Game class into Snake.

--- SnakeGame/Game.cs
+++ SnakeGame/Game.cs
@@ -7,21 +7,18 @@ namespace SnakeGame
     internal class Game
     {
         private RenderWindow _window;
-        private RectangleShape _snake;
-        private Vector2f _position = new Vector2f(100, 200);
-        private float _moveSpeed = 100f;
-        private readonly Clock _clock = new Clock();
-        private State _movementDirection;
+        private Snake _snake;
 
         public Game()
         {
             // Setup window
             _window = new RenderWindow(new VideoMode(800, 600), "Snake game");
-            _movementDirection = State.MovingRight;
+            _snake = new Snake();
         }
 
         public void Run()
         {
+
             // Main game loop
             while (_window.IsOpen)
             {
@@ -29,56 +26,13 @@ namespace SnakeGame
                 _window.DispatchEvents();
 
                 // Get elapsed time since clock was restarted.
-                float deltaTime = _clock.Restart().AsSeconds();
+                // float deltaTime = _clock.Restart().AsSeconds();
 
-                // Update game logic here
-                _snake = new RectangleShape(new Vector2f(10, 10))
-                {
-                    FillColor = SFML.Graphics.Color.Green,
-                    Position = _position
-                };
+                // Game logic
+                _snake.Move();
 
-                // Update state machine
-
-                switch (_movementDirection)
-                {
-                    case State.MovingLeft:
-                        _position.X -= _moveSpeed * deltaTime;
-                        if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
-                            _movementDirection = State.MovingUp;
-                        else if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
-                            _movementDirection = State.MovingDown;
-                        break;
-                    case State.MovingRight:
-                        _position.X += _moveSpeed * deltaTime;
-
-                        if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
-                            _movementDirection = State.MovingUp;
-                        else if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
-                            _movementDirection = State.MovingDown;
-                        break;
-                    case State.MovingUp:
-                        _position.Y -= _moveSpeed * deltaTime;
-
-                        if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
-                            _movementDirection = State.MovingLeft;
-                        else if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
-                            _movementDirection = State.MovingRight;
-                        break;
-                    case State.MovingDown:
-                        _position.Y += _moveSpeed * deltaTime;
-                        if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
-                            _movementDirection = State.MovingLeft;
-                        else if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
-                            _movementDirection = State.MovingRight;
-                        break;
-                }
-
-                _snake.Position = _position;
 
                 Render();
-
-                // handle collisions with apple and walls
             }
 
         }
@@ -86,21 +40,15 @@ namespace SnakeGame
         private void Render()
         {
             // Clear the window
-            _window.Clear(SFML.Graphics.Color.Black);
+            _window.Clear(Color.Black);
 
             // Draw game elements
-            _window.Draw(_snake);
+            _window.Draw(_snake.Body);
 
             // Display the contents of the window
             _window.Display();
         }
     }
 
-    enum State
-    {
-        MovingLeft,
-        MovingRight,
-        MovingUp,
-        MovingDown
-    }
+    
 }
--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -1,43 +1,118 @@
 ﻿using SFML.System;
 using SFML.Graphics;
 using SFML.Window;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SnakeGame
 {
     internal class Snake
     {
+        private float _moveSpeed = 100f;
+        private Direction _movementDirection = Direction.Right;
+        private Vector2f _position = new Vector2f(100, 200);
+        public RectangleShape Body;
+        public int LengthInSegments = 1;
+        public bool IsAlive;
+        private readonly Clock _clock = new Clock();
+        public float deltaTime;
+
+        // metoder: SetDirection, IncreaseLength, Eat, CheckCollision -> food / walls, Lose, Reset
+
         //public Vector2f Position = new Vector2f(100, 200);
         //public RectangleShape Body = new RectangleShape(new Vector2f(10, 10));
         //public int Length = 10;
         //private bool _isAlive = true;
         //private Direction _direction;
 
-        //public Snake()
-        //{
-        //    _direction = Direction.Right;
-        //    Body.FillColor = SFML.Graphics.Color.Green;
-        //}
+        public Snake()
+        {
+            IsAlive = true;
+            Body = new RectangleShape(new Vector2f(10, 10))
+            {
+                FillColor = Color.Green,
+                Position = _position
+            };
+        }
+
+        public void Move()
+        {
+            deltaTime = _clock.Restart().AsSeconds();
+            CheckDirection();
+            Body.Position = _position;
+            HandleInput();
+
+            // handle collisions with apple and walls
+        }
+
+        public void CheckDirection()
+        {
+            switch (_movementDirection)
+            {
+                case Direction.Left:
+                    MoveLeft();
+                    break;
+                case Direction.Right:
+                    MoveRight();
+                    break;
+                case Direction.Up:
+                    MoveUp();
+                    break;
+                case Direction.Down:
+                    MoveDown();
+                    break;
+            }
+        }
+
+        private void MoveDown()
+        {
+            _position.Y += _moveSpeed * deltaTime;
+        }
+
+        private void MoveUp()
+        {
+            _position.Y -= _moveSpeed * deltaTime;
+        }
+
+        private void MoveRight()
+        {
+            _position.X += _moveSpeed * deltaTime;
+        }
+
+        private void MoveLeft()
+        {
+            _position.X -= _moveSpeed * deltaTime;
+        }
+
+        private void HandleInput()
+        {
+            if (_movementDirection == Direction.Left || _movementDirection == Direction.Right)
+            {
+                if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
+                    _movementDirection = Direction.Up;
+                else if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
+                    _movementDirection = Direction.Down;
+            }
+            else if (_movementDirection == Direction.Down || _movementDirection == Direction.Up)
+            {
+                if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
+                    _movementDirection = Direction.Right;
+                else if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
+                    _movementDirection = Direction.Left;
+            }
+        }
+
 
         //private void Eat(IEdible edible)
         //{
         //    edible.GetEatenBy(this);
         //}
 
-        //public void SetDirection(Direction newDirection)
-        //{
-        //    // Prevent the snake from immediately reversing its direction
-        //    if ((_direction == Direction.Left && newDirection != Direction.Right) ||
-        //        (_direction == Direction.Right && newDirection != Direction.Left) ||
-        //        (_direction == Direction.Up && newDirection != Direction.Down) ||
-        //        (_direction == Direction.Down && newDirection != Direction.Up))
-        //    {
-        //        _direction = newDirection;
-        //    }
-        //}
     }
 
+    enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
 }
